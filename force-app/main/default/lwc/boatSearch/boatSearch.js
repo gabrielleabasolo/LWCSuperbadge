78d0e378b9817baff7ffdde5a9c5bf223cf4b35c
gabrielleabasolo <gabrielleabasolo@gmail.com>
2025-04-11 16:37:41 +0800

    Updated

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,23 +1,33 @@
- // imports
- export default class BoatSearch extends LightningElement {
-    isLoading = false;
-    
-    // Handles loading event
-    handleLoading() {
-        this.isLoading = true;
-     }
-    
-    // Handles done loading event
-    handleDoneLoading() {
+import { LightningElement } from 'lwc';
+import {naviagtionMixin} from 'lightning/navigation';
 
-     }
-    
-    // Handles search boat event
-    // This custom event comes from the form
-    searchBoats(event) {
-        
-     }
-    
-    createNewBoat() { }
+
+export default class BoatSearch extends LightningElement {  isLoading = false;
+  
+  // Handles loading event
+  handleLoading() {
+    this.isLoading = true;
   }
   
+  // Handles done loading event
+  handleDoneLoading() {
+    this.isLoading = false;}
+  
+  // Handles search boat event
+  // This custom event comes from the form
+  searchBoats(event) {
+    let boatTypeId = event.detail.boatTypeId;
+    this.template.querySelector('c-boat-search-results').searchBoats(boatTypeId);
+    this.handleDoneLoading();
+  }
+  
+  createNewBoat() {
+    this['navigationMixin.Navigate']({  
+    type: 'standard__objectPage', 
+    attributes: {
+      objectApiName: 'Boat__c',
+      actionName: 'new'
+    }
+    });
+  }
+}
